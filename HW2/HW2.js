baebaeb1416d4c4a09db7eba76ffc55e9ee2359c
mike <liwenzheng.mike@gmail.com>
2016-06-28 00:21:17 +0800

    update

--- HW2/HW2.js
+++ HW2/HW2.js
@@ -15,16 +15,11 @@ HMM.prototype.data = {
     //初始機率物件
     //init_state_prob
     init_prob: {
-        's1': 0.34,
+        's1': 0.33,
         's2': 0.33,
-        's3': 0.33
+        's3': 0.34
     },
 
-    // init_prob: {
-    //     's1': 0.5,
-    //     's2': 0.2,
-    //     's3': 0.3
-    // },
     //狀態轉移機率物件
     //transition_prob
     trans_prob: {
@@ -39,18 +34,7 @@ HMM.prototype.data = {
         's2': { 'A': 0.33, 'B': 0.34, 'C': 0.33 },
         's3': { 'A': 0.33, 'B': 0.33, 'C': 0.34 }
     }
-    // trans_prob: {
-    //     's1': { 's1': 0.6, 's2': 0.2, 's3': 0.2 },
-    //     's2': { 's1': 0.5, 's2': 0.3, 's3': 0.2 },
-    //     's3': { 's1': 0.4, 's2': 0.1, 's3': 0.5 }
-    // },
-    // //觀測符號出現機率
-    // //observations_prob 
-    // obs_prob: {
-    //     's1': { 'A': 0.7, 'B': 0.1, 'C': 0.2 },
-    //     's2': { 'A': 0.1, 'B': 0.6, 'C': 0.3 },
-    //     's3': { 'A': 0.3, 'B': 0.3, 'C': 0.4 }
-    // }
+
 }
 
 HMM.prototype.tempdata = {
@@ -67,16 +51,12 @@ HMM.prototype.tempdata = {
     //初始機率物件
     //init_state_prob
     init_prob: {
-        's1': 0.34,
+        's1': 0.33,
         's2': 0.33,
-        's3': 0.33
+        's3': 0.34
     },
 
-    // init_prob: {
-    //     's1': 0.5,
-    //     's2': 0.2,
-    //     's3': 0.3
-    // },
+
     //狀態轉移機率物件
     //transition_prob
     trans_prob: {
@@ -91,18 +71,7 @@ HMM.prototype.tempdata = {
         's2': { 'A': 0.33, 'B': 0.34, 'C': 0.33 },
         's3': { 'A': 0.33, 'B': 0.33, 'C': 0.34 }
     }
-    // trans_prob: {
-    //     's1': { 's1': 0.6, 's2': 0.2, 's3': 0.2 },
-    //     's2': { 's1': 0.5, 's2': 0.3, 's3': 0.2 },
-    //     's3': { 's1': 0.4, 's2': 0.1, 's3': 0.5 }
-    // },
-    // //觀測符號出現機率
-    // //observations_prob 
-    // obs_prob: {
-    //     '1': { 'A': 0.7, 'B': 0.1, 'C': 0.2 },
-    //     's2': { 'A': 0.1, 'B': 0.6, 'C': 0.3 },
-    //     's3': { 'A': 0.3, 'B': 0.3, 'C': 0.4 }
-    // }
+
 }
 HMM.prototype.setObservations = function(arr) {
     //以物件導向方式設定observations值
@@ -111,14 +80,27 @@ HMM.prototype.setObservations = function(arr) {
 }
 HMM.prototype.printInit = function() {
     //將模型內數學機率印出
+    this.tempdata = this.data;
     console.log("\n Init_state prob: \n", this.data.init_prob);
     console.log("\n Transition prob: \n", this.data.trans_prob);
     console.log("\n Observations prob: \n", this.data.obs_prob);
 }
 
 
+HMM.prototype.insideTest = function(arr) {
+
+    for (var i = 0; i < arr.length; i++) {
+        console.log("------\n");
+        this.setObservations(arr[i]);
+        console.log("Probility of " + arr[i].join());
+        console.log(ForwardQ1(this.data));
+    }
+
+
+}
+
 HMM.prototype.question3 = function(prob1, prob2) {
-        console.log("------");
+        console.log("------\n");
         this.setObservations(prob1);
         console.log("Probility of " + prob1.join());
         console.log(ForwardQ1(this.data));
@@ -130,67 +112,143 @@ HMM.prototype.question3 = function(prob1, prob2) {
 
     }
     //訓練
-HMM.prototype.trainning = function(prob) {
-    this.setObservations(prob);
-    console.log("\n" + prob.join());
-    // console.log(this.data.obs);
-    //Forward
-    // Alpha(this.data, 2, 's1');
-    // console.log("  - ");
-    // Beta(this.data, 2, 's1');
+HMM.prototype.trainning = function(trainningData) {
 
-    // console.log("  - ");
-    // console.log("\nForwardQ1: ");
-    // console.log(ForwardQ1(this.data));
-    // console.log(Alpha(this.data, 2, 's1') * Beta(this.data, 2, 's1') + Alpha(this.data, 2, 's2') * Beta(this.data, 2, 's2') + Alpha(this.data, 2, 's3') * Beta(this.data, 2, 's3'));
-    // console.log(ksin(this.data, 2, 's1', 's1')+ksin(this.data, 2, 's1', 's2')+ksin(this.data, 2, 's1', 's3'));
-    // console.log(gama(this.data, 2, 's1') );
+    var L = trainningData.length;
     var ipatten, jpatten, obpatten;
-    for (var i = 0; i < this.data.states.length; i++) {
-        ipatten = this.data.states[i];
-        for (var j = 0; j < this.data.states.length; j++) {
-            jpatten = this.data.states[j];
-            this.tempdata.trans_prob[ipatten][jpatten] = trainningA(this.data, ipatten, jpatten);
-            console.log("After trainning a[" + ipatten + "][" + jpatten + "]=" + trainningA(this.data, ipatten, jpatten));
+
+console.log("- after trainning-\n");
+
+    for (var i = 0; i < this.tempdata.states.length; i++) {
+        ipatten = this.tempdata.states[i];
+        var _pi = 0;
+
+        for (var x = 0; x < L; x++) {
+            this.setObservations(trainningData[x]);
+
+            _pi += gama(this.tempdata, 1, ipatten);
         }
+        // console.log(accDiv(_pi, L));
+        _pi = accDiv(_pi, L);
+
+        this.tempdata.init_prob[ipatten] = _pi;
+          console.log("pi[" + ipatten + "]=" + _pi);
+    }
+
+
+
+
+
+
+
+
+    console.log("\n");
+
+    //trainning a
+
+    for (var i = 0; i < this.tempdata.states.length; i++) {
+        ipatten = this.tempdata.states[i];
+        for (var j = 0; j < this.tempdata.states.length; j++) {
+            jpatten = this.tempdata.states[j];
+
+            var _aEl = 0;
+            var _aDe = 0;
+            for (var x = 0; x < L; x++) {
+                this.setObservations(trainningData[x]);
+                // console.log(trainningData[x]);
+                _aEl += trainningAEl(this.tempdata, ipatten, jpatten);
+                _aDe += trainningADe(this.tempdata, ipatten, jpatten);
+
+            }
+            
+            // for (var x = 0; x < L; x++) {
+            //     this.setObservations(trainningData[x]);
+            //     _aDe += trainningADe(this.tempdata, ipatten, jpatten);
+
+            // }
+
+
+            this.tempdata.trans_prob[ipatten][jpatten] = accDiv(_aEl, _aDe);
+            console.log("a[" + ipatten + "][" + jpatten + "]=" + accDiv(_aEl, _aDe));
+
+        }
+    }
+
+    function trainningAEl(data, i, j) {
+        var ans = 0;
+        var base = 0;
+       
+        for (var x = 0; x < data.obs.length-1; x++)
+            ans = ans + ksin(data, x+1, i, j);
+
+        return ans;
+    }
+
+    function trainningADe(data, i, j) {
+        var ans = 0;
+        var base = 0;
+       
+
+        for (var x = 0; x < data.obs.length-1; x++)
+            base = base + gama(data, x+1, i);
+
+
+
+        return base;
     }
 
     console.log("\n");
 
-    for (var j = 0; j < this.data.states.length; j++) {
-        jpatten = this.data.states[j];
-        for (var ob = 0; ob < this.data.states.length; ob++) {
+
+    //trainning b
+ 
+
+    for (var j = 0; j < this.tempdata.states.length; j++) {
+        jpatten = this.tempdata.states[j];
+        for (var ob = 0; ob < this.tempdata.states.length; ob++) {
             if (ob == 0) obpatten = 'A';
             else if (ob == 1) obpatten = 'B';
             else if (ob == 2) obpatten = 'C';
-            this.tempdata.obs_prob[jpatten][obpatten] = trainningB(this.data, jpatten, obpatten);
-            console.log("After trainning b[" + ipatten + "][" + obpatten + "]=" + trainningB(this.data, jpatten, obpatten));
+            // obpatten=this.tempdata.states[ob];
+
+            var _bEl = 0;
+            for (var x = 0; x < L; x++) {
+                this.setObservations(trainningData[x]);
+                _bEl += trainningBEl(this.tempdata, jpatten, obpatten);
+
+            }
+            var _bDe = 0;
+            for (var x = 0; x < L; x++) {
+                this.setObservations(trainningData[x]);
+                _bDe += trainningBDe(this.tempdata, jpatten, obpatten);
+
+            }
+
+          
+            this.tempdata.obs_prob[jpatten][obpatten] = accDiv(_bEl, _bDe);
+
+            console.log("b[" + jpatten + "][" + obpatten + "]=" + accDiv(_bEl, _bDe));
         }
     }
 
-    console.log("\n");
 
-    for (var i = 0; i < this.data.states.length; i++) {
-        ipatten = this.data.states[i];
-        this.tempdata.init_prob[ipatten] = trainningPi(this.data, ipatten);
-        console.log("After trainning Pi[" + ipatten + "]=" + trainningPi(this.data, ipatten));
-
-    }
 
     console.log("\n");
     console.log("P(O|~λ) " + ForwardQ1(this.tempdata));
     console.log("P(O|λ)  " + ForwardQ1(this.data));
 
-    if (ForwardQ1(this.tempdata) > ForwardQ1(this.data))
-        this.data = this.tempdata;
-
+    // if (ForwardQ1(this.tempdata) > ForwardQ1(this.data))
+    this.data = this.tempdata;
+    // console.log(this.data);
     console.log("- section-");
 }
 
 
 
 
-function trainningA(data, i, j) {
+
+
+function trainningAUp(data, i, j) {
     var ans = 0;
     var base = 0;
     // ksin(this.data, 2, 's1', 's2')
@@ -207,10 +265,9 @@ function trainningA(data, i, j) {
 }
 
 
-function trainningB(data, j, o) {
+function trainningBEl(data, j, o) {
     var ans = 0;
     var boo;
-    var base = 0;
     // // ksin(this.data, 2, 's1', 's2')
     // // ksin(this.data, t, i, j)
     for (var x = 1; x < data.obs.length; x++) {
@@ -219,21 +276,28 @@ function trainningB(data, j, o) {
         ans = ans + boo * gama(data, x, j);
     }
 
+
+    return ans;
+}
+
+
+function trainningBDe(data, j, o) {
+
+    var boo;
+    var base = 0;
+    // // ksin(this.data, 2, 's1', 's2')
+    // // ksin(this.data, t, i, j)
+
+
     for (var x = 1; x < data.obs.length; x++)
         base = base + gama(data, x, j);
 
-    ans = accDiv(ans, base);
 
-    return ans;
+
+    return base;
 }
 
 
-function trainningPi(data, i) {
-    var ans = 0;
-    ans = gama(data, 1, i);
-
-    return ans;
-}
 
 
 function ForwardQ1(data) {
@@ -416,10 +480,14 @@ function Beta(data, start, state) {
 
 
 function ksin(data, t, i, j) {
-    var ans = 1;
+    var ans = 1.00;
+    // console.log(data);
+    //  console.log( i);
+    //  console.log(j);
 
     ans = accMul(ans, Alpha(data, t, i));
     ans = accMul(ans, data.trans_prob[i][j]);
+    // console.log(data.obs[t]);
     ans = accMul(ans, data.obs_prob[j][data.obs[t]]);
     ans = accMul(ans, Beta(data, t + 1, j));
     // console.log(" - ");
@@ -427,28 +495,40 @@ function ksin(data, t, i, j) {
     // console.log(data.trans_prob[i][j]);
     // console.log(data.obs_prob[j][data.obs[t]]);
     // console.log(Beta(data, t + 1, j));
-    var base = 0;
-    for (var o = 0; o < data.states.length; o++) {
-        j = data.states[o];
-        base += Alpha(data, data.obs.length, j);
-    }
+    // var base = 0;
+    // for (var o = 0; o < data.states.length; o++) {
+    //     j = data.states[o];
+    //     base += Alpha(data, data.obs.length, j);
+    // }
 
     // console.log(base);
-    // ans = accDiv(ans, ForwardQ1(data));
-    ans = accDiv(ans, base);
+    ans = accDiv(ans, ForwardQ1(data));
+    // ans = accDiv(ans, base);
 
     return ans;
 }
 
 function gama(data, t, i) {
+    // console.log(data);
     var ans = 0;
     var j;
+    // for (var o = 0; o < data.states.length; o++) {
+    //     j = data.states[o];
+    //     ans = ans + ksin(data, t, i, j)
+    // }
+    // console.log(ans);
+
+    ans = 1;
+    var bs = 0;
+    ans = accMul(ans, Alpha(data, t, i));
+    ans = accMul(ans, Beta(data, t, i));
     for (var o = 0; o < data.states.length; o++) {
         j = data.states[o];
-        ans = ans + ksin(data, t, i, j)
+        bs += accMul(Alpha(data, t, j), Beta(data, t, j));
     }
-
-
+    ans = accDiv(ans, bs);
+    // console.log(ans);
+    // console.log("-----");
 
     return ans;
 }
@@ -543,7 +623,7 @@ function Viterbi(data) {
 
 
 //浮點數乘法相乘運算
-function accMul(x, y) {
+function accMul1(x, y) {
     var m = 0,
         s1 = x.toString(),
         s2 = y.toString();
@@ -552,10 +632,15 @@ function accMul(x, y) {
     return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m)
 }
 
-function accMul0(x, y) {
+function accMul(x, y) {
     var c = Math.log(x) + Math.log(y);
     return Math.exp(c);
 }
+
+
+
+
+
 
 function accDiv(x, y) {
     var t1 = 0,
@@ -571,29 +656,31 @@ function accDiv(x, y) {
 }
 
 
-// var LZERO = -1.0E10; // ~log(0)
-// var LSMALL = -0.5E10; // log values < LSMALL are set to LZERO
-// var minLogExp = -Math.log(-LZERO); // ~=-23
+var LZERO = -1.0E10; // ~log(0)
+var LSMALL = -0.5E10; // log values < LSMALL are set to LZERO
+var minLogExp = -Math.log(-LZERO); // ~=-23
 // console.log(LZERO);
 // console.log(LSMALL);
 // console.log(minLogExp);
 
 
-// function accLogAdd(x, y) {
-//     var temp, diff, z;
-//     if (x < y) {
-//         temp = x;
-//         x = y;
-//         y = temp;
-//     }
-//     diff = y - x; //notice that diff <= 0
-//     if (diff < minLogExp) // if y’ is far smaller than x’
-//         return (x < LSMALL) ? LZERO : x;
-//     else {
-//         z = Math.exp(diff);
-//         return x + Math.log(1.0 + z);
-//     }
-// }
+function accLogAdd(x, y) {
+    x = Math.log(x);
+    y = Math.log(y);
+    var temp, diff, z;
+    if (x < y) {
+        temp = x;
+        x = y;
+        y = temp;
+    }
+    diff = y - x; //notice that diff <= 0
+    if (diff < minLogExp) // if y’ is far smaller than x’
+        return (x < LSMALL) ? LZERO : x;
+    else {
+        z = Math.exp(diff);
+        return x + Math.log(1.0 + z);
+    }
+}
 
 
 
@@ -608,34 +695,67 @@ function accDiv(x, y) {
 // 7. CABCABCA
 // 8. CABCA
 // 9. CABCA
+var insideTest1Arr = [
+    ['A', 'B', 'B', 'C', 'A', 'B', 'C', 'A', 'A', 'B', 'C'],
+    ['A', 'B', 'C', 'A', 'B', 'C'],
+    ['A', 'B', 'C', 'A', 'A', 'B', 'C'],
+    ['B', 'B', 'A', 'B', 'C', 'A', 'B'],
+    ['B', 'C', 'A', 'A', 'B', 'C', 'C', 'A', 'B'],
+    ['C', 'A', 'C', 'C', 'A', 'B', 'C', 'A'],
+    ['C', 'A', 'B', 'C', 'A', 'B', 'C', 'A'],
+    ['C', 'A', 'B', 'C', 'A'],
+    ['C', 'A', 'B', 'C', 'A']
+];
+
+var insideTest2Arr = [
+    ['B', 'B', 'B', 'C', 'C', 'B', 'C'],
+    ['C', 'C', 'B', 'A', 'B', 'B'],
+    ['A', 'A', 'C', 'C', 'B', 'B', 'B'],
+    ['B', 'B', 'A', 'B', 'B', 'A', 'C'],
+    ['C', 'C', 'A', 'A', 'B', 'B', 'A', 'B'],
+    ['B', 'B', 'B', 'C', 'C', 'B', 'A', 'A'],
+    ['A', 'B', 'B', 'B', 'B', 'A', 'B', 'A'],
+    ['C', 'C', 'C', 'C', 'C'],
+    ['B', 'B', 'A', 'A', 'A']
+];
+
+// hmmModel1 = new HMM();
+// hmmModel1.printInit();
+// for (var times = 0; times < 50; times++) {
+// hmmModel1.trainning([
+//     ['A', 'B', 'B', 'C', 'A', 'B', 'C', 'A', 'A', 'B', 'C'],
+//     ['A', 'B', 'C', 'A', 'B', 'C'],
+//     ['A', 'B', 'C', 'A', 'A', 'B', 'C'],
+//     ['B', 'B', 'A', 'B', 'C', 'A', 'B'],
+//     ['B', 'C', 'A', 'A', 'B', 'C', 'C', 'A', 'B'],
+//     ['C', 'A', 'C', 'C', 'A', 'B', 'C', 'A'],
+//     ['C', 'A', 'B', 'C', 'A', 'B', 'C', 'A'],
+//     ['C', 'A', 'B', 'C', 'A'],
+//     ['C', 'A', 'B', 'C', 'A']
+// ]);}
+// hmmModel1.insideTest(insideTest1Arr);
+// hmmModel1.insideTest(insideTest2Arr);
+// hmmModel1.question3(['A', 'B', 'C', 'A', 'B', 'C', 'C', 'A', 'B'], ['A', 'A', 'B', 'A', 'B', 'C', 'C', 'C', 'C', 'B', 'B', 'B']);
 
 
-hmmModel1 = new HMM();
-hmmModel1.printInit();
-
-hmmModel1.trainning(['A', 'B', 'B', 'C', 'A', 'B', 'C', 'A', 'A', 'B', 'C']);
-hmmModel1.trainning(['A', 'B', 'C', 'A', 'B', 'C']);
-hmmModel1.trainning(['A', 'B', 'C', 'A', 'A', 'B', 'C']);
-hmmModel1.trainning(['B', 'B', 'A', 'B', 'C', 'A', 'B']);
-hmmModel1.trainning(['B', 'C', 'A', 'A', 'B', 'C', 'C', 'A', 'B']);
-hmmModel1.trainning(['C', 'A', 'C', 'C', 'A', 'B', 'C', 'A']);
-hmmModel1.trainning(['C', 'A', 'B', 'C', 'A', 'B', 'C', 'A']);
-hmmModel1.trainning(['C', 'A', 'B', 'C', 'A']);
-hmmModel1.trainning(['C', 'A', 'B', 'C', 'A']);
-hmmModel1.question3(['A', 'B', 'C', 'A', 'B', 'C', 'C', 'A', 'B'], ['A', 'A', 'B', 'A', 'B', 'C', 'C', 'C', 'C', 'B', 'B', 'B']);
 
 
+ hmmModel2 = new HMM();
+ hmmModel2.printInit();
+for (var times = 0; times < 50; times++) {
+hmmModel2.trainning([
+    ['B', 'B', 'B', 'C', 'C', 'B', 'C'],
+    ['C', 'C', 'B', 'A', 'B', 'B'],
+    ['A', 'A', 'C', 'C', 'B', 'B', 'B'],
+    ['B', 'B', 'A', 'B', 'B', 'A', 'C'],
+    ['C', 'C', 'A', 'A', 'B', 'B', 'A', 'B'],
+    ['B', 'B', 'B', 'C', 'C', 'B', 'A', 'A'],
+    ['A', 'B', 'B', 'B', 'B', 'A', 'B', 'A'],
+    ['C', 'C', 'C', 'C', 'C'],
+    ['B', 'B', 'A', 'A', 'A']
+ ]);
+ }
 
-// hmmModel2 = new HMM();
-// hmmModel2.printInit();
-
-// hmmModel2.trainning(['B', 'B', 'B', 'C', 'C', 'B', 'C']);
-// hmmModel2.trainning(['C', 'C', 'B', 'A', 'B', 'B']);
-// hmmModel2.trainning(['A', 'A', 'C', 'C', 'B', 'B', 'B']);
-// hmmModel2.trainning(['B', 'B', 'A', 'B', 'B', 'A', 'C']);
-// hmmModel2.trainning(['C', 'C', 'A', 'A', 'B', 'B', 'A', 'B']);
-// hmmModel2.trainning(['B', 'B', 'B', 'C', 'C', 'B', 'A', 'A']);
-// hmmModel2.trainning(['A', 'B', 'B', 'B', 'B', 'A', 'B', 'A']);
-// hmmModel2.trainning(['C', 'C', 'C', 'C', 'C']);
-// hmmModel2.trainning(['B', 'B', 'A', 'A', 'A']);
-// hmmModel2.question3(['A', 'B', 'C', 'A', 'B', 'C', 'C', 'A', 'B'], ['A', 'A', 'B', 'A', 'B', 'C', 'C', 'C', 'C', 'B', 'B', 'B']);
+hmmModel2.insideTest(insideTest1Arr);
+hmmModel2.insideTest(insideTest2Arr);
+ hmmModel2.question3(['A', 'B', 'C', 'A', 'B', 'C', 'C', 'A', 'B'], ['A', 'A', 'B', 'A', 'B', 'C', 'C', 'C', 'C', 'B', 'B', 'B']);
